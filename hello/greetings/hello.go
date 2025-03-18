96d8468ef9d8c2a3c42d55b8b140522fb2b9d975
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-03-18 16:58:06 +0300

    first day

---
+++ hello/greetings/hello.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Go())
+
+}
+func a() {
+
+}
+
+func b() {
+
+}
+func c() {
+
+}
+func d() {
+
+}
---
+++ home/greetings/greetings.go
@@ -0,0 +1,13 @@
+package greetings
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+
+	/*var valMessage string
+	valMessage = "Hi, " + name + ". Welcome!"*/
+	return message
+}
