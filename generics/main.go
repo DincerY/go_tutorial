d8c5eb3bf24e57e9d6f3021cad51e444e51ab075
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-03-27 16:48:49 +0300

    seventh day

--- generics/main.go
+++ generics/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
+// SumNumbers sums the values of map m. It supports both integers
+// and floats as map values.
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
 func main() {
 	// Initialize a map for the integer values
 	ints := map[string]int64{
@@ -20,8 +34,12 @@ func main() {
 		SumFloats(floats))
 
 	fmt.Printf("Generic Sums: %v and %v\n",
-		SumIntsOrFloats[string, int64](ints),
-		SumIntsOrFloats[string, float64](floats))
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+
+	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
 }
 
 // SumInts adds together the values of m.
